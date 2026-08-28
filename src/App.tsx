cc1fd268b793e1e72270f5c4b71044f3ebb4ff3d
Nidel <nidelouzid3@gmail.com>
2026-08-28 18:17:35 +0200

    Build reusable task cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 // import { Navbar } from "./components/Navbar";
-// import { TaskCard } from "./components/TaskCard";
+import { Navbar } from "./components/Navbar";
+import { TaskCard } from "./components/TaskCard";
+import type { Task } from "./types/tasks";
 
-import TaskForm from "./components/TaskForm";
+// import TaskForm from "./components/TaskForm";
 
 function App(){
+  const tasks : Task[]=[
+    {
+      id: 1,
+      title: "Learn React",
+      description: "Practice React components and props",
+      priority: "high",
+      completed: false,
+      dueDate: "2026-08-30",
+    },
+    {
+      id: 2,
+      title: "Build Express API",
+      description: "Create my first Express API",
+      priority: "medium",
+      completed: false,
+      dueDate: "2026-09-02",
+    },
+    {
+      id: 3,
+      title: "Learn MongoDB",
+      description: "Learn how to store data with MongoDB",
+      priority: "low",
+      completed: true,
+      dueDate: "2026-09-05",
+    },
+
+  ]
   return(
     <div>
-      {/* <Navbar/>
-      <TaskCard/> */}
-      <TaskForm/>
+      <Navbar/>
+      <div className="flex justify-around  min-h-screen mt-20 ">
+        {tasks.map((task)=>(
+        <TaskCard key={task.id} task={task}/>
+        ))}
+        
+        </div>      
     </div>
   )
 }
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,43 +1,26 @@
+import type { Task } from "../types/tasks"
 
-export const TaskCard = () => {
+interface TaskCardProps{
+    task: Task;
+}
+export const TaskCard: React.FC<TaskCardProps> = ({task}) => {
   return (
-    <section className="flex justify-around min-h-screen mt-20 ">
-        <div className="card flex flex-col  text-purple-600/75 border  border-purple-500 size-70 space-y-4  pl-4">
-            <h1 className="text-xl pt-4">Name of practise:</h1>
-            <h4 className="text-base">Status:...</h4>
-            <h5 className="text-sm">Ratings:...</h5>
-            <h3 className="text-lg">Date of production : ...</h3>
-            <p className="text-sm">Comments: ...</p>
-            <div className="flex justify-around ">
+    <section >
+        <div className="card flex flex-col  text-purple-600/75 border  border-purple-500 size-80 space-y-4  pl-4">
+            <h1 className="text-xl pt-4">id: {task.id}</h1>
+            <h4 className="text-base">title: {task.title}</h4>
+            <h5 className="text-sm">description: {task.description}</h5>
+            <h3 className="text-lg">priority: {task.priority}</h3>
+            <p className="text-sm">completed: {task.completed}</p>
+            <p className="text-sm">dueDate: {task.dueDate}</p>
+            <div className="flex justify-around mt-auto mb-4">
                 <button className="btn btn-active btn-accent">Edit</button>
                 <button className="btn btn-active btn-accent">Delete</button>
             </div>
 
         </div>
 
-        <div className="card flex flex-col  text-purple-600/75 border  border-purple-500 size-70 space-y-4 pl-4 ">
-            <h1 className="text-xl pt-4">Name of practise:</h1>
-            <h4 className="text-base">Status:...</h4>
-            <h5 className="text-sm">Ratings:...</h5>
-            <h3 className="text-lg">Date of production : ...</h3>
-            <p className="text-sm">Comments: ...</p>
-            <div className="flex justify-around ">
-                <button className="btn btn-active btn-accent">Edit</button>
-                <button className="btn btn-active btn-accent">Delete</button>
-            </div>
-        </div>
-
-        <div className="card flex flex-col  text-purple-600/75 border  border-purple-500 size-70 space-y-4 pl-4 ">
-            <h1 className="text-xl pt-4">Name of practise:</h1>
-            <h4 className="text-base">Status:...</h4>
-            <h5 className="text-sm">Ratings:...</h5>
-            <h3 className="text-lg">Date of production : ...</h3>
-            <p className="text-sm">Comments: ...</p>
-            <div className="flex justify-around ">
-                <button className="btn btn-active btn-accent">Edit</button>
-                <button className="btn btn-active btn-accent">Delete</button>
-            </div>
-        </div>
+        
     </section>
   )
 }
